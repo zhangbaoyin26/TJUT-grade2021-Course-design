e07596a42f7f55b089d5384761d41136dcb43426
张宝印 <98701392+zhangbaoyin26@users.noreply.github.com>
2023-01-07 14:46:01 +0800

    Add files via upload

---
+++ USER/main.c
@@ -0,0 +1,148 @@
+/********************START***********************
+ * 项目：天津理工大学大二上学期项目制作——防疫检测
+ * 模块：stm32f103c8t6, 
+ *        0.96oled, 
+ *        JDY-31-SPP蓝牙模块,
+ *        HC-SR04超声波测距
+ * 
+ * 接线：
+ *       stm32f103c8t6 -> 0.96oled:
+ *          GND         ->  GND
+ *          VCC(3.3v)   ->  VCC
+ *          PA5         ->  SCL
+ *          PA6         ->  SDA
+ *       stm32f103c8t6 -> HC-SR04超声波测距:
+ *          VCC(5v)     ->  VCC
+ *          PB8         ->  Trig
+ *          PB9         ->  Echo
+ *          GND         ->  GND
+ *       stm32f103c8t6 -> JDY-31-SPP蓝牙模块：
+ *          PA7         ->  STATE
+ *          PA9         ->  RXD
+ *          PA10        ->  TXD
+ *          GND         ->  GND
+ *          VCC(5V)     ->  VCC
+ *          <float>     ->  EN
+ * 编译环境：
+ * 
+ ********************END************************/
+
+
+#include "stm32f10x.h"
+#include "usart\usart.h"
+#include "SysTick\bsp_SysTick.h"
+#include "stm32f10x_it.h"
+#include "oled\oled.h"
+#include "TimBase\bsp_TiMbase.h"
+#include "EXIT\bsp_exti.h"
+#include "HCSR04\hcsr.h"
+#include "delay\delay.h"
+
+#define CLI() __set_PRIMASK(1)				/* 关总中断 */  
+#define SEI() __set_PRIMASK(0)				/* 开总中断 */ 
+
+volatile u32 triger_time = 0; // 10us triger计时变量,>10us
+volatile u32 echo_time = 0;   // 10us echo计时变量, =距离*2/声速
+u32 distance_time = 0;				// 10us 记录echo高电平时间
+
+uint8_t echo_flag = 0;				// echo_flag=1 ->接收到回波信号
+
+uint8_t global_arr[100];
+void delay(uint32_t tim){
+  uint32_t i = 0;
+  for (; i < tim; ++i);
+}
+
+
+
+int main(){
+  // 初始化SysTick
+  // 中断初始化
+  USART_Config();
+
+  // oled初始化
+  delay_init();         //延时函数初始化
+  NVIC_Configuration(); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+  delay_ms(8000);
+  GPIO_Conf();
+  OLED_Init(); //初始化OLED
+  OLED_Clear();
+
+  // 开始执行main函数主体
+  // 检查是否连接蓝牙，如果没有连接，就一直等待蓝牙连接
+  while(GPIO_ReadInputDataBit(STATUS_PORT, STATUS_Pin) == 0){
+    OLED_ShowString(16, 2, "Disconnect", 16);
+  }
+  OLED_Clear();
+  OLED_ShowString(16, 2, "Connecting...", 16);
+  delay_ms(20000);
+  OLED_Clear();
+  OLED_ShowString(16, 0, "Connected", 16);
+
+  // 初始化超声波
+  HCSR_GPIO_Config();
+  EXTI_Pxy_Config(); 		//配置PB9为上升沿中断，PB9->Echo
+  /* 通用定时器 TIMx,x[2,3,4,5] 定时配置 */	
+    TIMx_Configuration();
+	/* 配置通用定时器 TIMx,x[2,3,4,5]的中断优先级 */
+	TIMx_NVIC_Configuration();
+	macTIM_APBxClock_FUN (macTIM_CLK, ENABLE);
+
+  // 循环
+  float dis = 0;
+  uint32_t cnt = 0;
+  uint8_t  disarr[13];
+  disarr[0]='d';
+  disarr[1]='i';
+  disarr[2]='s';
+  disarr[3]=':';
+  disarr[4]='0';
+  disarr[5]='0';
+  disarr[6]='0';
+  disarr[7]='.';
+  disarr[8]='0';
+  disarr[9]='0';
+  disarr[10]='c';
+  disarr[11]='m';
+  OLED_ShowString(16, 3, disarr, 16);
+  while (1)
+  {
+    // 检测是否有物体靠近
+    Triger();
+		echo_time = 0;
+		echo_flag = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9);
+		while(echo_flag == 0)				//等待收到回波信号
+		{
+			echo_flag = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9);
+		}
+		echo_time = 0;
+		while(echo_flag == 1)						//等待回波信号接收完成
+		{
+			echo_flag = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9);
+		}
+		distance_time = echo_time;
+		dis = Calculation();
+    if (dis>0.02 && dis<=0.1)   // 在最低阈值2cm以上，10cm内，有物体靠近
+    {
+      cnt++;
+      printf("Somebody nearly: {time:%d, count:%d, distance:%.3fcm}", 000000, cnt, dis*100);
+    }
+    else    // 没有物体靠近
+    {
+      
+    }
+    // u8 INT_dis = (int)(dis*10000);
+      disarr[4] = (uint8_t)(dis*1)%10+'0';
+      disarr[5] = (uint8_t)(dis*10)%10+'0';
+      disarr[6] = (uint8_t)(dis*100)%10+'0';
+      disarr[7] = '.';
+      disarr[8] = (uint8_t)(dis*1000)%10+'0';
+      disarr[9] = (uint8_t)(dis*10000)%10+'0';
+      OLED_ShowString(16, 3, disarr, 16);
+    delay_ms(10000);
+  }
+}
+
+// void SystemInit(){
+//   //
+// }
---
+++ USER/stm32f10x_it.c
@@ -0,0 +1,205 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c 
+  * @author  MCD Application Team
+  * @version V3.4.0
+  * @date    10/15/2010
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @copy
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_it.h"
+#include "TimBase\bsp_TiMbase.h"
+#include "EXIT\bsp_exti.h"
+
+extern volatile u32 echo_time; 
+extern volatile u32 triger_time;
+
+/** @addtogroup STM32F10x_StdPeriph_Template
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M3 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief   This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+}
+
+/*
+ * 串口usart1中断服务函数
+ * @param   None
+ * @retval  None
+ */
+void DEBUG_USART_IRQHandler(void)
+{
+  extern uint8_t global_arr[100];
+  uint8_t ucTemp;
+  u8 i = 0;
+	if(USART_GetITStatus(DEBUG_USARTx,USART_IT_RXNE)!=RESET)
+	{		
+		ucTemp = USART_ReceiveData(DEBUG_USARTx);
+    global_arr[i] = ucTemp;
+    ++i;
+    USART_SendData(DEBUG_USARTx, ucTemp);    
+	}	
+}
+
+//  超声波中断函数 * 2
+void  macTIM_INT_FUN (void)
+{
+	if ( TIM_GetITStatus( macTIMx, TIM_IT_Update) != RESET ) 
+	{	
+		triger_time++;
+		echo_time++;
+		TIM_ClearITPendingBit( macTIMx , TIM_FLAG_Update);  		 
+	}		 	
+}
+
+///IO线中断，线中断连接PB9
+void macEXTI_INT_FUNCTION (void)
+{
+	if(EXTI_GetITStatus(macEXTI_LINE) != RESET) 
+	{
+		macTIM_APBxClock_FUN (macTIM_CLK, ENABLE);
+		echo_time = 0;
+		EXTI_ClearITPendingBit(macEXTI_LINE);     
+	}  
+}
+
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+/**
+  * @}
+  */ 
+
+
+/******************* (C) COPYRIGHT 2010 STMicroelectronics *****END OF FILE****/
